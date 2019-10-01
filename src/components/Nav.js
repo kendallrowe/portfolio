08be198e3c91f2f840e3b861254cb87be1494ed2
Kendall Rowe <kendall.rowe312@gmail.com>
2019-10-01 15:09:06 -0400

    Set up Nav component and style sheet

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import "./Nav.scss";
+
+function Nav() {
+  return (
+    <ul>
+      <li>Profile</li>
+      <li>Projects</li>
+    </ul>
+  );
+}
+
+export default Nav;
