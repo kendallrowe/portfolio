9307ccd91a48d5a71990a5a3fc138d72b3414f31
kendallrowe <kendall.rowe312@gmail.com>
2019-11-24 16:36:51 -0500

    Set up wowjs and animate.css

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,15 +6,16 @@ import "./index.scss";
 function About() {
 
   return (
-    <div id="about">
+    <section id="about" className="wow bounceInUp">
       <div className="aboutKendall">
-        <h2>About</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae nesciunt quaerat vitae, cumque architecto placeat quia veniam id fuga corrupti quam atque quo, et fugit impedit, modi nihil voluptatum voluptatem.
-        Quia, consequatur non. Veritatis sit libero rerum, perspiciatis nisi voluptatibus quae eligendi velit ullam dolorem quidem debitis nam aut impedit incidunt consequatur! Quae culpa cum dolore quibusdam obcaecati eius fuga!
-        Consectetur atque maiores, praesentium ab voluptatibus maxime ipsa nostrum magni, tempora nihil accusantium animi nulla placeat laboriosam earum, soluta reprehenderit. Doloremque, quos possimus commodi dignissimos fugit repudiandae voluptatibus quam quod.</p>
+        <h2>About Me</h2>
+        <p>A full stack developer with a background in business - as you can see I very literally think outside the box.</p>
+        <p>Having graduated from Ryerson University with a Bachelor's of Commerce majoring in Economics and Management Science, I began my career as an Associate within TD Securities.</p>
+        <p>After completing the esteemed two year Leadership Associate Program within Global Operations and Business Services, I decided to pursue my passion for code. After completing the program, I took a leave of absence to undertake the full stack web bootcamp at Lighthouse Labs in June 2019.</p>
+        <p>I am now a Python developer at TD Securities, and have been trusted within a team of three to build </p>
       </div>
       <AboutCube/>
-    </div>
+    </section>
   );
 }
 
