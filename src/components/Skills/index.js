2ee9f3ed246d78e82050018c860f6255b19b058d
kendallrowe <kendall.rowe312@gmail.com>
2019-11-29 23:26:00 -0500

    Reordered skills and added cursor pointer to skills section

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -21,11 +21,11 @@ function Skills({ skills, selectSkill }) {
       <h2>Skills</h2>
       <div className="skill-badge-container">
         <Badge 
-          iconType={faReact}
+          iconType={faJsSquare}
           allSelected={allSelected}
-          onClick={e => selectSkill("React")}
-          label="React"
-          selected={skills.indexOf("React") !== -1}/>
+          onClick={e => selectSkill("JavaScript")}
+          label="JavaScript"
+          selected={skills.indexOf("JavaScript") !== -1}/>
         <Badge 
           iconType={faPython}
           allSelected={allSelected}
@@ -33,17 +33,17 @@ function Skills({ skills, selectSkill }) {
           label="Python"
           selected={skills.indexOf("Python") !== -1}/>
         <Badge 
-          iconType={faJsSquare}
+          iconType={faGem}
           allSelected={allSelected}
-          onClick={e => selectSkill("JavaScript")}
-          label="JavaScript"
-          selected={skills.indexOf("JavaScript") !== -1}/>
+          onClick={e => selectSkill("Ruby")}
+          label="Ruby"
+          selected={skills.indexOf("Ruby") !== -1}/> 
         <Badge 
-          iconType={faCss3Alt}
+          iconType={faReact}
           allSelected={allSelected}
-          onClick={e => selectSkill("CSS3")}
-          label="CSS3"
-          selected={skills.indexOf("CSS3") !== -1}/>
+          onClick={e => selectSkill("React")}
+          label="React"
+          selected={skills.indexOf("React") !== -1}/>
       </div>
       <div className="skill-badge-container">
         <Badge 
@@ -52,6 +52,12 @@ function Skills({ skills, selectSkill }) {
           onClick={e => selectSkill("HTML5")}
           label="HTML5"
           selected={skills.indexOf("HTML5") !== -1}/>
+        <Badge 
+          iconType={faCss3Alt}
+          allSelected={allSelected}
+          onClick={e => selectSkill("CSS3")}
+          label="CSS3"
+          selected={skills.indexOf("CSS3") !== -1}/>
         <Badge 
           iconType={faGitSquare}
           allSelected={allSelected}
@@ -64,12 +70,6 @@ function Skills({ skills, selectSkill }) {
           onClick={e => selectSkill("Node")}
           label="Node"
           selected={skills.indexOf("Node") !== -1}/>
-        <Badge 
-          iconType={faGem}
-          allSelected={allSelected}
-          onClick={e => selectSkill("Ruby")}
-          label="Ruby"
-          selected={skills.indexOf("Ruby") !== -1}/> 
       </div>
       <a className={resumeClasses} href="#resume">Resume</a>
     </section>
